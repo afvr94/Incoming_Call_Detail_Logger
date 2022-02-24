f6bf1734f99afab498672c3ee9faf7df6f91b269
afvr94 <abdiel017@gmail.com>
2022-02-24 11:34:06 -0500

    feat: added a print

--- call_detail_logger.py
+++ call_detail_logger.py
@@ -67,7 +67,7 @@ def detect_COM_port():
     proc = subprocess.Popen(['ls /dev/tty[A-Za-z]*'], shell=True, stdout=subprocess.PIPE)
     com_ports = proc.communicate()[0]
     com_ports_list = com_ports.split('\n')
-    print(com_ports_list)
+    print(proc.communicate())
 
     # Find the right port associated with the Voice Modem
     for com_port in com_ports_list:
